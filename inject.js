1bb3f827a73a7a7ff07809a35e7816ffeb27016b
Kevin Kang <kevink97@cs.washington.edu>
2017-03-18 23:03:44 -0700

    partially working inject.js

--- inject.js
+++ inject.js
@@ -1,21 +1,25 @@
 console.log("injected");
 
+var msg = [];
 window.onload = function() {
 	var lookedAt = false;
 	$('.uiScrollableAreaContent').on("DOMNodeInserted", function() {
-		var msg = [];
-			var messages = document.getElementsByClassName("_58nk");
-			for (var i = 0; i < messages.length; i++) {
-				msg.push(messages[i].innerText);
-			}
-			msg = parseData(msg);
-			console.log(msg);
-			lookedAt = true;
-
-			// var sentiments = getSentiments(msg);
-	});
+			scrapeMessages();
+		});
 };
 
+function scrapeMessages() {
+	var msg = [];
+	var messages = document.getElementsByClassName("_58nk");
+	for (var i = 0; i < messages.length; i++) {
+		msg.push(messages[i].innerText);
+	}
+	msg = parseData(msg);
+	console.log(msg);
+	lookedAt = true;
+
+	// var sentiments = getSentiments(msg);
+}
 
 function parseData(msg) {
 	return msg;
