634b6c0246a4f3bc69a961985eb69e96a9a2e73a
Kevin Kang <kevink97@cs.washington.edu>
2017-03-19 07:12:05 -0700

    working messenger sentiment checker

--- inject.js
+++ inject.js
@@ -14,13 +14,13 @@ function injectSentiments(data, min, max) {
 	var sentiments = data;
 	for (var i = min; i < max; i++) {
 		var sent = parseFloat(sentiments[i - min].toString().replace(/ /g,'')); 
-		var add = "<b>[Sentiment: " + sent + " ";
-		if (sent < 0.1) { add = add + ":sob:";}
-		else if (sent < 0.3) {add = add + ":disappointed:";}
-		else if (sent < 0.5) { add = add + ":worried:";}
-		else if (sent < 0.7) { add = add + ":simple_smile:";}
-		else { add = add + ":relaxed:";}
-		add = add.concat(" ]</b>");
+		var add = "<b>[Sentiment: dafadfasfdfasdfaf" + " ";
+		if (sent < 0.1) { add = add + "\u1F62D";}
+		else if (sent < 0.3) {add = add + "\u1F625";}
+		else if (sent < 0.5) { add = add + "\u1F610";}
+		else if (sent < 0.7) { add = add + "\u1F60A";}
+		else { add = add + "\u1F618";}
+		add = add + " ]</b>";
 		if (!messages[min + i].innerHTML.includes("<b>[Sentiment: ")) {
 			messages[min + i].innerHTML = messages[min + i].innerText + " " + add;
 		}
@@ -30,12 +30,14 @@ function injectSentiments(data, min, max) {
 function injectSentiment(data, index) {
 	var messages = document.getElementsByClassName("_58nk");
 	var sent = parseFloat(data.toString().replace(/ /g,'')); 
-	var add = "<b>[Sentiment: " + sent + " ";
-	if (sent < 0.1) { add = add + ":sob:";}
-	else if (sent < 0.3) {add = add + ":disappointed:";}
-	else if (sent < 0.5) { add = add + ":worried:";}
-	else if (sent < 0.7) { add = add + ":simple_smile:";}
-	else { add = add + ":relaxed:";}
+	var add = "<b>[Sentiment: " + " ";
+	if (isNaN(sent)) {
+		add = add + "service temporarily unavailable";
+	} else if (sent < 0.1) { add = add + "<img class=\"_1ift _1ifu img\" src=\"https://static.xx.fbcdn.net/images/emoji.php/v8/zfa/1/32/1f62d.png\">";}
+	else if (sent < 0.3) {add = add + "<img class=\"_1ift _1ifu img\" src=\"https://static.xx.fbcdn.net/images/emoji.php/v8/z4b/1/32/1f625.png\">";}
+	else if (sent < 0.5) { add = add + "<img class=\"_1ift _1ifu img\" src=\"https://static.xx.fbcdn.net/images/emoji.php/v8/z27/1/32/1f610.png\">";}
+	else if (sent < 0.7) { add = add + "<img class=\"_1ift _1ifu img\" src=\"https://static.xx.fbcdn.net/images/emoji.php/v8/z6/1/32/1f642.png\">";}
+	else { add = add + "<img class=\"_1ift _1ifu img\" src=\"https://static.xx.fbcdn.net/images/emoji.php/v8/z8e/1/32/1f606.png\">";}
 	add = add.concat(" ]</b>");
 	if (!messages[index].innerHTML.includes("<b>[Sentiment: ")) {
 		messages[index].innerHTML = messages[index].innerText + " " + add;
